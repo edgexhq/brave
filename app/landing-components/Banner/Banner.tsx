be6bbd7a29076369fc84e01fee8cf62fd34e6c8e
Anish <xevenbiswas@gmail.com>
2024-06-29 13:57:26 +0530

    hero changes

--- app/landing-components/Banner/Banner.tsx
+++ app/landing-components/Banner/Banner.tsx
@@ -7,11 +7,14 @@ const Banner = () => {
   return (
     <main>
       <div className="px-6 lg:px-8">
-        <Blobby className="bg-blue-400/60 top-12 left-10" />
-        <div className="mx-auto max-w-7xl pt-16 sm:pt-20 pb-20 banner-image">
+        <Blobby className="bg-purple-500/60 top-12 left-10" />
+        <Blobby className=" bottom-20 left-40 bg-yellow-400/50 " />
+        <Blobby className="bg-green-400/60 top-12 right-10" />
+        <Blobby className="bg-cyan-500/60 bottom-3 right-52" />
+        <div className="mx-auto max-w-7xl pt-16 sm:pt-16 pb-20 banner-image">
           <div className="text-center">
-            <h1 className="text-4xl font-bold text-navyblue sm:text-5xl lg:text-7xl md:4px">
-              A Next Gen Perfect <br /> Interview Prep Suite.
+            <h1 className="text-4xl font-bold max-w-4xl mx-auto text-navyblue sm:text-5xl lg:text-7xl md:4px">
+              Power your interviews with <span className="text-primary">Brave</span>
             </h1>
             <p className="mt-6 text-lg max-w-prose text-center mx-auto leading-8 text-bluegray">
               At Brave, we revolutionize the way you prepare for and conduct
