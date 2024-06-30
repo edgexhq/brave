6f0d2d522ffb1ef60ea667e47e61982ec0254de1
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2024-06-30 10:22:54 +0530

    feat: update interview feature

--- app/dashboard/interview/page.tsx
+++ app/dashboard/interview/page.tsx
@@ -6,13 +6,19 @@ import InterviewList from "@/components/interview/interview-list";
 export default function InterviewDashboard() {
   return (
     <div className="p-5">
-      <h2 className="font-bold text-3xl text-black mb-2">mock interview.</h2>
-      <h2 className="text-gray-500">
-        Create and Start your AI Mock Interview
-      </h2>
+      <div className="h-36 bg-gradient-to-tl from-green-300 via-blue-500 to-purple-600 p-10 rounded-lg mb-10">
+        <div className="flex items-center justify-start gap-x-10 mb-10">
+          <div>
+            <h2 className="font-bold text-3xl text-white mb-2">
+              mock interview.
+            </h2>
+            <h2 className="text-gray-200">
+              Create and Start your AI Mock Interview
+            </h2>
+          </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 my-5 gap-5">
-        <AddNewInterview />
+          <AddNewInterview />
+        </div>
       </div>
 
       {/* Previous Interview List  */}
